7d22f3f935794586394557dc884b48a0932e18f9
ThibaultVercoutre <thibault.vercoutre@yahoo.com>
2022-12-02 12:33:50 +0100

    petites modifications

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ console.log(pageAffichage.getAttribute("data-value"));
 
 function resetAllMenu(elts) {
     for(var i = 0; i < elts.length; i++) {
-        elts[i].style.backgroundColor = "transparent";
+        elts[i].style.backgroundColor = "grey";
     }
 }
 
