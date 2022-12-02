60080eb4857e9e0b001ee8326ff89b7657851da7
ThibaultVercoutre <thibault.vercoutre@yahoo.com>
2022-12-02 17:56:11 +0100

    design changement de pages

--- script/script.js
+++ script/script.js
@@ -1,10 +1,28 @@
 // ================================================
+// ================== Variable ====================
+
+var page_active = "0";
+
+// ================================================
+// ================== Initialisation de la page ===
+
+window.onload = () => {
+    document.querySelector("#Accueil").style.backgroundColor = "whitesmoke";
+    document.querySelector("#page-accueil").style.display = "block";
+    pages_types = document.querySelectorAll(".pages-types");
+    for(var i = 0; i < pages_types.length; i++){
+        pages_types[i].style.display = "none";
+    }
+};
+
+// ================================================
 // ================== Clique menu =================
 
 let boutonsMenu = document.querySelectorAll("nav .bouton");
-let pageAffichage = document.querySelector("#pages");
+let Pages = document.getElementById("pages").childNodes;
+console.log(Pages);
 
-console.log(pageAffichage.getAttribute("data-value"));
+let pageAffichage = document.querySelector("#pages");
 
 function resetAllMenu(elts) {
     for(var i = 0; i < elts.length; i++) {
@@ -12,9 +30,33 @@ function resetAllMenu(elts) {
     }
 }
 
+function resetAllPage(p){
+    for(var i = 1; i < p.length; i += 2) {
+        p[i].style.display = "none";
+    }
+}
+
 for(var i = 0; i < boutonsMenu.length; i++){
     boutonsMenu[i].onclick = function(e){
+        page_active = e.target.getAttribute("data-value");
+
         resetAllMenu(boutonsMenu);
         e.target.style.backgroundColor = "whitesmoke";
+        
+        resetAllPage(Pages);
+        Pages[Number(page_active)*2+1].style.display = "block";
+
+        
+    }
+}
+
+// ================================================
+// ================== Clique type exos ============
+
+let boutonsTypesExos = document.querySelectorAll("nav .bouton_type_exo");
+
+for(var i = 0; i < boutonsTypesExos.length; i++){
+    boutonsTypesExos[i].onclick = function(e){
+        console.log(e.target);
     }
 }
