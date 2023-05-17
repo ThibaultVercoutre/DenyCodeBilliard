bd3cea9c5d3122d2451f16f79391094d58e6205b
ThibaultVercoutre <thibault.vercoutre@yahoo.com>
2023-05-17 16:52:13 +0200

    1.5

--- pages/versions/v1.5.php
+++ pages/versions/v1.5.php
@@ -1,4 +1,5 @@
-<h1>V1.5 - Ajout des interactions entre utilisateur (15 mai 2023)</h1>
+<h1>V1.5 - Ajout des interactions entre utilisateur (17 mai 2023)</h1>
 
 <h3>Les amis</h3>
 <p>Vous pouvez maintenant ajouter des amis, à vous d'accepter les demandes ou pas !</p>
+<p>De plus, vous avez la possibilité de leur parler via un chat. Seuls les messages textuels fonctionnent<p>
